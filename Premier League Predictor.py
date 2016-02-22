1baa78c54bab8c7388ef7ecef0cba4e1111dc051
ZackAkil <zackakil@Zacks-MacBook-Pro.local>
2016-02-22 10:48:52 +0000

    update author name

--- Premier League Predictor.py
+++ Premier League Predictor.py
@@ -1,4 +1,4 @@
-# Premier league predictor Search script ~ By Zackary Akil 2014 
+# Premier league predictor Search script ~ By Zack Akil 2014 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
